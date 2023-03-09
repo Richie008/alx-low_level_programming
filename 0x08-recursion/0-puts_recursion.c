2974e0cfa2699aa9c95b908b03099653dd43a572
Richard <richardabia74@gmail.com>
2023-03-09 01:50:00 -0800

    hey c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,27 +1,21 @@
+#include "main.h"
+#include <unistd.h>
 
-/*                                                                                                                                      
- * File: 0-puts_recursion.c                                                                                                             
- * Auth: sam                                                                                                                            
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * _puts_recursion - Prints a string, followed by a new line.                                                                           
- * @s: The string to be printed.                                                                                                        
- */                                                                                                                                     
-void _puts_recursion(char *s)                                                                                                           
-{                                                                                                                                       
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                _putchar(*s);                                                                                                           
-                _puts_recursion(s + 1);                                                                                                 
-        }                                                                                                                               
-                                                                                                                                        
-        else                                                                                                                            
-                _putchar('\n');                                                                                                         
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+    if (*s)
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else
+    {
+        _putchar('\n');
+    }
 }
-
-
-
 
